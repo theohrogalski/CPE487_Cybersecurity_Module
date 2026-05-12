8addd1381c7378349729ed6eb1ba6a0bb22c4ea1
theohrogalski <151564012+theohrogalski@users.noreply.github.com>
2026-05-12 12:57:24 -0400

    Add files via upload

---
+++ ps2receiver.vhd
@@ -0,0 +1,85 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use ieee.numeric_std.all;
+entity PS2RECEIVER is port(
+clk : in std_logic;
+kclk : in std_logic;
+kdata :in std_logic;
+keycodeout : out std_logic_vector(31 downto 0)
+
+);
+end PS2receiver;
+
+architecture keyboard of PS2RECEIVER is 
+    signal datacur : std_logic_vector(7 downto 0);
+    signal dataprev : std_logic_vector(7 downto 0);   
+    signal keycode :std_logic_vector(31 downto 0):=X"00000000";
+    signal flag : std_logic:='0';
+    begin
+process(kclk)
+variable cnt : integer:=0;
+variable release : std_logic :='1';
+begin 
+if falling_edge(kclk) then
+    case(cnt) is
+    when 0 =>
+    cnt := cnt+1;
+    when 1 => 
+    datacur(0)<=kdata;
+    cnt:=cnt+1;
+
+    when 2 => 
+    datacur(1)<=kdata;    
+        cnt:=cnt+1;
+
+    when 3 => 
+    datacur(2)<=kdata;
+        cnt:=cnt+1;
+
+    when 4 =>
+     datacur(3)<=kdata;
+         cnt:=cnt+1;
+
+    when 5 =>
+     datacur(4)<=kdata;    
+     cnt:=cnt+1;
+
+    when 6 => 
+    datacur(5)<=kdata;
+        cnt:=cnt+1;
+
+    when 7 => 
+    datacur(6)<=kdata;
+        cnt:=cnt+1;
+
+    when 8 => 
+    datacur(7)<=kdata;
+        cnt:=cnt+1;
+
+    when 9 => 
+    release:='1';
+    if datacur = x"f0" then 
+     release:='0';
+    end if;
+    
+    if release = '1' then
+    if (datacur/=dataprev) then 
+    
+        dataprev <= datacur;
+        keycode <=  keycode(23 downto 0) & datacur;
+    cnt:=cnt+1;
+    end if;
+    end if;
+    when 10 => 
+    cnt := 0;
+    
+    when others => null;
+    end case;
+    end if;
+end process;
+
+keycodeout<=keycode;
+ 
+end keyboard;
---
+++ top.vhd
@@ -0,0 +1,57 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+-- Top component
+entity top is 
+port(
+    CLK100MHZ : in std_logic;
+    PS2_CLK : in std_logic;
+    PS2_DATA:in std_logic;
+    SEG : out std_logic_vector(6 downto 0);
+    AN  : out std_logic_vector(7 downto 0);
+    DP : out std_logic;
+   UART_TXD : out std_logic
+  --  keycode : out std_logic_vector(31 downto 0)
+);
+end entity top;
+--Addition
+-- Keyboard component
+
+architecture top_arc of top is 
+
+component clk_wiz_0_clk_wiz is 
+port(
+clk_in1 :in std_logic;
+clk_out1: out std_logic
+);
+end component;
+
+component seg7display is
+port (
+x : in std_logic_vector(31 downto 0);
+clk : in std_logic;
+SEG : out std_logic_vector(6 downto 0);
+AN : out std_logic_vector (7 downto 0);
+DP : out std_logic
+);
+end component;
+
+component ps2receiver is 
+port (
+clk : in std_logic;
+kclk : in std_logic;
+kdata :in std_logic;
+keycodeout : out std_logic_vector(31 downto 0)
+);
+
+end component;
+signal clocksignal : std_logic;
+signal keycode_signal : std_logic_vector(31 downto 0);
+begin 
+clock : clk_wiz_0_clk_wiz port map (clk_in1=>clk100mhz, clk_out1=>clocksignal);
+keyboard : ps2receiver port map (clk=>clocksignal, kclk=>PS2_CLK, kdata=>PS2_DATA, keycodeout=>keycode_signal);
+display : seg7display port map (x=>keycode_signal,clk=>clk100mhz,DP=>DP,AN=>AN,SEG=>SEG);
+
+end top_arc; 
