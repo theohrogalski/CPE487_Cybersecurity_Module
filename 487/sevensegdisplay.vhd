c153d6fbfcc52d5c9ac01c321ce0fdb3c513e84b
theohrogalski <151564012+theohrogalski@users.noreply.github.com>
2026-05-12 13:21:27 -0400

    Add files via upload

---
+++ 487/sevensegdisplay.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/10/2026 09:03:09 PM
+-- Design Name: 
+-- Module Name: sevensegdisplay - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sevensegdisplay is
+--  Port ( );
+end sevensegdisplay;
+
+architecture Behavioral of sevensegdisplay is
+
+begin
+
+
+end Behavioral;
